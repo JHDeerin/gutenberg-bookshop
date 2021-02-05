d59b896ab47d12e622ce6ee3d3fd429819d31fce
Jake Deerin <2016deerinjo@gmail.com>
2021-02-04 20:35:21 -0500

    Minor refactoring of book interactions into functions

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -22,7 +22,29 @@ func _ready():
 	
 	book_open_sound = $BookOpenSound
 	page_turn_sound = $PageTurnSound
+
+
+func set_description(description: String):
+	description_label.text = description
+
+
+func _process(_delta):
+	if is_book_visible and Input.is_action_just_pressed("ui_cancel"):
+		close_book()
+	book_display.visible = is_book_visible
 	
+	if Input.get_mouse_mode() != Input.MOUSE_MODE_VISIBLE:
+		return
+		
+	if Input.is_action_just_pressed("ui_right"):
+		flip_page(true)
+	if Input.is_action_just_pressed("ui_left"):
+		flip_page(false)
+	update_book_page(book_text_label, page_count_label)
+
+# TODO: All the functions beneath should be moved to the book display (except
+# possibly the book downloading action)
+
 func display_book(book_id: int):
 	is_book_visible = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -30,33 +52,34 @@ func display_book(book_id: int):
 	print("Displaying ", book_id)
 	book_open_sound.play_random_sound()
 	book_downloader.download_ebook_text(book_id)
-	
-func set_description(description: String):
-	description_label.text = description
 
-func _process(_delta):
-	if is_book_visible and Input.is_action_just_pressed("ui_cancel"):
-		is_book_visible = false
-		book_open_sound.play_random_sound()
-	book_display.visible = is_book_visible
-	
-	if Input.get_mouse_mode() != Input.MOUSE_MODE_VISIBLE:
-		return
-		
-	if Input.is_action_just_pressed("ui_right"):
-		# TODO: Refactor this so these actions are being done on the object itself?
+
+func close_book():
+	is_book_visible = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	book_open_sound.play_random_sound()
+
+
+func flip_page(is_flipping_right: bool):
+	"""
+	Flips the text of the book forwards/backwards to the next page of content
+	"""
+	# TODO: Refactor this so these actions are being done on the object itself?
+	if is_flipping_right:
 		book_text_label.lines_skipped += book_text_label.max_lines_visible
 		book_text_scroll.scroll_vertical = 0
-		page_turn_sound.play_random_sound()
-		
-	if Input.is_action_just_pressed("ui_left"):
-		# TODO: Refactor this so these actions are being done on the object itself
+	else:
 		book_text_label.lines_skipped -= book_text_label.max_lines_visible
 		book_text_scroll.scroll_vertical = 1e9
-		page_turn_sound.play_random_sound()
+	page_turn_sound.play_random_sound()
 
+
+func update_book_page(book_text_label: Label, page_count_label: Label):
+	"""
+	TODO: This should be done on the book text node itself
+	"""
 	book_text_label.lines_skipped = int(clamp(book_text_label.lines_skipped, 0, book_text_label.get_line_count()))
 	var current_page = (book_text_label.lines_skipped / book_text_label.max_lines_visible) + 1
 	var max_page = (book_text_label.get_line_count() / book_text_label.max_lines_visible) + 1
 	page_count_label.text = "Page %d/%d" % [current_page, max_page]
-		
+	
